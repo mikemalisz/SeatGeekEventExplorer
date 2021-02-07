bb3c13021b3fa41e7e587ea9e7c0533de5f369aa
Mike Maliszewski <mikemalisz@gmail.com>
2021-02-06 20:51:40 -0600

    Implemented disk storage service

--- SeatGeekEventExplorer/Utility/DiskStorageService.swift
+++ SeatGeekEventExplorer/Utility/DiskStorageService.swift
@@ -10,12 +10,62 @@ import Foundation
 class DiskStorageService {
     static let shared = DiskStorageService()
     
-    let dictionaryStore = [String: Any]()
+    /// Container for stored values this object manages
+    var store = StorageContainer()
+    
+    // MARK: - Object Lifecycle
     
     private init() {
+        updateLocalStoreFromDisk()
+    }
+    
+    deinit {
+        saveLocalStoreToDisk()
+    }
+    
+    // MARK: - Disk Storage Handlers
+    
+    private func updateLocalStoreFromDisk() {
+        let fileURL = FileManager
+            .getDocumentsDirectory()
+            .appendingPathComponent(Constants.storageFilename)
+        
+        do {
+            let data = try Data(contentsOf: fileURL)
+            let decodedStore = try JSONDecoder().decode(StorageContainer.self, from: data)
+            store = decodedStore
+        } catch {
+            print(error)
+            assertionFailure()
+        }
+    }
+    
+    private func saveLocalStoreToDisk() {
+        let fileURL = FileManager
+            .getDocumentsDirectory()
+            .appendingPathComponent(Constants.storageFilename)
+        
+        do {
+            let encodedData = try JSONEncoder().encode(store)
+            try encodedData.write(to: fileURL)
+        } catch {
+            print(error)
+            assertionFailure()
+        }
         
     }
     
+    // MARK: - Types
+    
+    struct StorageContainer: Codable {
+        var favoritedKeys: Set<String>
+        
+        init(favoritedKeys: Set<String> = .init()) {
+            self.favoritedKeys = favoritedKeys
+        }
+    }
+    
     private struct Constants {
+        static let storageFilename = "DiskStorageFile"
     }
 }

3b94debc7be0ff5bd7ec71e0f1c53a990e41bd14
Mike Maliszewski <mikemalisz@gmail.com>
2021-02-07 13:29:21 -0600

    added documentation to live event retrieving

--- SeatGeekEventExplorer/NetworkService/LiveEventRetrieving.swift
+++ SeatGeekEventExplorer/NetworkService/LiveEventRetrieving.swift
@@ -10,5 +10,12 @@ import Foundation
 protocol LiveEventRetrieving {
     typealias LiveEventServerResponse = (Result<[LiveEvent], Error>) -> Void
     
+    /// Retrieves events with the given query from the server
+    ///
+    /// - Parameters:
+    ///   - query: The query to provide the server for matching events
+    ///   - completionHandler: A closure called once the server request has completed
+    ///
+    /// completionHandler may be called on a background thread
     func retrieveEvents(with query: String?, completionHandler: @escaping LiveEventServerResponse)
 }

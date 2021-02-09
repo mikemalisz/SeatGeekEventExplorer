110d6101d96eb3f8d88553a5784936ddd999709a
Mike Maliszewski <mikemalisz@gmail.com>
2021-02-08 21:38:14 -0600

    created basic layout and class for live event list cell

---
+++ SeatGeekEventExplorer/LiveEventList/LiveEventListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LiveEventListTableViewCell.swift
+//  SeatGeekEventExplorer
+//
+//  Created by Mike Maliszewski on 2/8/21.
+//
+
+import UIKit
+
+class LiveEventListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

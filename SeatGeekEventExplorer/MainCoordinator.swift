cc83dc0a2b492bf495d267ed4388f5704240637e
Mike Maliszewski <mikemalisz@gmail.com>
2021-02-08 20:30:08 -0600

    created main coordinator class

---
+++ SeatGeekEventExplorer/MainCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  MainCoordinator.swift
+//  SeatGeekEventExplorer
+//
+//  Created by Mike Maliszewski on 2/8/21.
+//
+
+import UIKit
+
+class MainCoordinator {
+    let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        
+    }
+    
+    func showDetail(for event: LiveEvent) {
+        
+    }
+}

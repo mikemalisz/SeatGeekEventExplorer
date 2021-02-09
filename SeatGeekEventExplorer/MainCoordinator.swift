8492d52b516569b7e317ce6eee74b5311fc57a94
Mike Maliszewski <mikemalisz@gmail.com>
2021-02-08 21:18:37 -0600

    implemented start on main coordinator

--- SeatGeekEventExplorer/MainCoordinator.swift
+++ SeatGeekEventExplorer/MainCoordinator.swift
@@ -15,7 +15,13 @@ class MainCoordinator {
     }
     
     func start() {
+        let eventProvider = SeatGeekNetworkService()
+        let eventListManager = LiveEventListManager(eventProvider: eventProvider,
+                                                    storageProvider: DiskStorageService.shared)
         
+        let controller = LiveEventListTableViewController()
+        controller.eventListManager = eventListManager
+        navigationController.pushViewController(controller, animated: false)
     }
     
     func showDetail(for event: LiveEvent) {

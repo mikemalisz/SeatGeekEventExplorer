e41e1c1388de948c699b5b8f926208869a89bc48
Mike Maliszewski <mikemalisz@gmail.com>
2021-02-09 14:07:56 -0600

    began implementing live event details view controller

---
+++ SeatGeekEventExplorer/LiveEventDetails/LiveEventDetailsViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LiveEventDetailsViewController.swift
+//  SeatGeekEventExplorer
+//
+//  Created by Mike Maliszewski on 2/9/21.
+//
+
+import UIKit
+
+class LiveEventDetailsViewController: UIViewController {
+    
+    // MARK: - Event Details
+    
+    var eventDetailsManager: LiveEventDetailsManager!
+    
+    private var event: LiveEvent {
+        eventDetailsManager.event
+    }
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet private weak var detailImageView: UIImageView!
+    
+    @IBOutlet private weak var dateLabel: UILabel!
+    
+    @IBOutlet private weak var locationLabel: UILabel!
+    
+    private func configureViewFromEventDetails() {
+        dateLabel.text = event.dateScheduled.description
+        
+        let location = "\(event.venue.city), \(event.venue.state)"
+        locationLabel.text = location
+    }
+    
+    // MARK: - Controller Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewFromEventDetails()
+    }
+
+}
